7f1968f360f47f0d2bbeea5e7bd9af8eddccd9b5
Shahriar Alif <fahim.shahriar.alif@gmail.com>
2026-07-25 23:50:19 +0600

    feat : added empty screens

---
+++ lib/screens/spending_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SpendingTab extends StatelessWidget {
+  const SpendingTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Page 2 - Spending')),
+    );
+  }
+}
