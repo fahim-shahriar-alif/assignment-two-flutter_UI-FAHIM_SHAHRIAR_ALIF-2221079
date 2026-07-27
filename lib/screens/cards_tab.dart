ee8bcb92438e07214db666ca4992664f5289d648
Shahriar Alif <fahim.shahriar.alif@gmail.com>
2026-07-27 12:07:42 +0600

    Initiated the Cards page

--- lib/screens/cards_tab.dart
+++ lib/screens/cards_tab.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import '../widgets/top_bar_widget.dart';
 
 class CardsTab extends StatelessWidget {
   const CardsTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Page 3 - Cards')),
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.00),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const TopBarWidget(),
+              const SizedBox(height: 20.00),
+              const Text('My Cards',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.00)),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
